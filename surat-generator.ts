81b269bc20d06c8207002089075c8909d4b97fc8
peculiarnewbie <32804415+peculiarnewbie@users.noreply.github.com>
2023-05-05 00:17:50 +0700

    1 binaries uploaded

--- surat-generator.ts
+++ surat-generator.ts
@@ -1,3 +1,4 @@
+import * as fs from "fs";
 import {
   AlignmentType,
   Document,
@@ -7,6 +8,8 @@ import {
   TabStopPosition,
   TabStopType,
   TextRun,
+  Header,
+  ImageRun,
 } from 'docx';
 const PHONE_NUMBER = '07534563401';
 const PROFILE_URL = 'https://www.linkedin.com/in/dolan1';
@@ -18,6 +21,45 @@ export class DocumentCreator {
     const document = new Document({
       sections: [
         {
+          headers: {
+            default: new Header({
+              children: [
+                new Paragraph({
+                  children: [
+                    new ImageRun({
+                      data: fs.readFileSync('./demo/images/image1.jpeg'),
+                      transformation: {
+                        width: 100,
+                        height: 100,
+                      },
+                    }),
+                  ],
+                }),
+                new Paragraph({
+                  children: [
+                    new ImageRun({
+                      data: fs.readFileSync('./demo/images/pizza.gif'),
+                      transformation: {
+                        width: 100,
+                        height: 100,
+                      },
+                    }),
+                  ],
+                }),
+                new Paragraph({
+                  children: [
+                    new ImageRun({
+                      data: fs.readFileSync('./demo/images/image1.jpeg'),
+                      transformation: {
+                        width: 100,
+                        height: 100,
+                      },
+                    }),
+                  ],
+                }),
+              ],
+            }),
+          },
           children: [
             this.createHeaderSurat(nomor),
             new Paragraph({
